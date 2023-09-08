aa28474b3f33c8063fffee617bdb81312c9c2ccb
Subash0711 <109809088+Subash0711@users.noreply.github.com>
2023-09-08 14:08:22 +0530

    Added changes

--- blog/blog_project/blog_project/settings.py
+++ blog/blog_project/blog_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-+lq-%+avk2f(ejg4z3to78mu1zic6b64=v%m0n!&h**#ja_7#0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
